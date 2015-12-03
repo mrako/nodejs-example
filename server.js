e06b7c8f78866f80191148d3ffaa28b1f9b52e61
Marko Klemetti <marko.klemetti@gmail.com>
2015-12-03 15:04:34 +0200

    Updated clustering

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-'use strict';
+var morgan = require('morgan');
 
 var server = require('./app');
 
+var cluster = require('cluster');
+
 var port = process.env.PORT || 8080;
+var workers = require('os').cpus().length;
 
-server.listen(port);
+server.use(morgan('dev', {immediate: true}));
 
-console.log('App listening on port ' + port);
+if (cluster.isMaster) {
+  for (var i = 1; i < workers; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('Worker ' + worker.process.pid + ' died');
+    console.log('Spawining new worker...');
+    cluster.fork();
+  });
+} else {
+  server.listen(port);
+}
+
+console.log("App listening on port " + port);
